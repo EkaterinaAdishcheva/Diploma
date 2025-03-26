e2fbcfe3da8d618c69f7c2c44e8234774979ed76
Your Name <you@example.com>
2025-03-26 21:34:40 +0000

    mult file version

--- inference.py
+++ inference.py
@@ -32,10 +32,13 @@ def projector_inference(projector_path, h_target, h_base, device):
         delta_emb_all = projector(mid_base_all[:,-1].to(device))
     return delta_emb_all
 
-def pipeline_inference(pipeline, config, oneactor_extra_config, generator=None):
+def pipeline_inference(pipeline, prompt, neg_prompt, config, oneactor_extra_config, generator=None):
     if generator is None:
         generator = torch.manual_seed(config['seed'])
-    return pipeline(config['prompt'], negative_prompt=config['neg_prompt'], num_inference_steps=config['inference_steps'], guidance_scale=config['eta_1'], \
+    return pipeline(
+            prompt,
+            negative_prompt=neg_prompt,
+            num_inference_steps=config['inference_steps'], guidance_scale=config['eta_1'], \
             generator=generator, oneactor_extra_config=oneactor_extra_config)
 
 def main():
@@ -63,39 +66,66 @@ def main():
     h_base = load_pickle(config['data_root']+'/base/mid_list.pkl')
     h_tar = xt_dic['h_mid']
 
-    # original output by SDXL
-    generator = torch.manual_seed(config['seed'])
-    image = pipeline(config['prompt'], negative_prompt=config['neg_prompt'], num_inference_steps=config['inference_steps'], guidance_scale=config['eta_1'], generator=generator)
-    image = image.images[0]
-    image.save(out_root+'/original_sdxl.jpg')
+    # iterate over image list
+    for img_num in range(len(config['add_prompts'])):
+        # original output by SDXL
+        generator = torch.manual_seed(config['seed'])
+        image = pipeline(
+            config['prompt'] + " " + config['add_prompts'][img_num], negative_prompt=config['neg_prompt'][img_num],
+            num_inference_steps=config['inference_steps'], guidance_scale=config['eta_1'], generator=generator)
+        image = image.images[0]
+        image.save(out_root+'/original_'+config['file_name'][img_num]+'_sdxl.jpg')
 
-    # perform step-wise guidance
-    select_steps = config['select_steps']
-    if select_steps is not False:
-        assert (len(select_steps) % 2) == 0
-        select_list = []
-        for _ in range(len(select_steps) // 2):
-            a = select_steps[2*_]
-            b = select_steps[2*_ + 1]
-            select_list = select_list + list(range(a-1,b))
-    else:
-        select_list = None
+        # perform step-wise guidance
+        select_steps = config['select_steps']
+        if select_steps is not False:
+            assert (len(select_steps) % 2) == 0
+            select_list = []
+            for _ in range(len(select_steps) // 2):
+                a = select_steps[2*_]
+                b = select_steps[2*_ + 1]
+                select_list = select_list + list(range(a-1,b))
+        else:
+            select_list = None
+        print(select_list)
 
-    # locate the base token id
-    token_id = find_token_ids(pipeline.tokenizer, config['prompt'], [config['base']])
-    generator = torch.manual_seed(config['seed'])
-    config['generator'] = generator
+        # locate the base token id
+        token_id = find_token_ids(pipeline.tokenizer, config['prompt'] + " " + config['add_prompts'][img_num], config['base'])
+        generator = torch.manual_seed(config['seed'])
+        config['generator'] = generator
 
-    if config['only_step'] is False:
-        for i in range(50):
-            steps = config['step_from']+config['step']*(i)
-            
-            projector_path = config['pretrain_root'] + f'/weight/learned-projector-steps-{steps}.pth'
+        if config['only_step'] is False:
+            for i in range(50):
+                steps = config['step_from']+config['step']*(i)
+                
+                projector_path = config['pretrain_root'] + f'/weight/learned-projector-steps-{steps}.pth'
+                delta_emb_all = projector_inference(projector_path, h_tar, h_base, config['device']).to(config['device'])
+
+                delta_emb_aver = delta_emb_all[:-1].mean(dim=0)
+                delta_emb_tar = config['v'] * delta_emb_all[-1]
+
+                oneactor_extra_config = {
+                    'token_ids': token_id,
+                    'delta_embs': delta_emb_tar,
+                    'delta_steps': select_list,
+                    'eta_2': config['eta_2'],
+                    'delta_emb_aver': delta_emb_aver
+                }
+
+                image = pipeline_inference(
+                    pipeline, 
+                    config['prompt'] + " " + config['add_prompts'][img_num],
+                    config['neg_prompt'][img_num],
+                    config, oneactor_extra_config)
+                image = image.images[0]
+                image.save(out_root+f'/OneActor_'+config['file_name'][img_num]+'_step_'+steps+'.jpg')
+        elif config['only_step'] == 'best':
+            projector_path = config['pretrain_root'] + f'/weight/best-learned-projector.pth'
             delta_emb_all = projector_inference(projector_path, h_tar, h_base, config['device']).to(config['device'])
 
-            delta_emb_aver = delta_emb_all[:-1].mean(dim=0)
-            delta_emb_tar = config['v'] * delta_emb_all[-1]
-
+            delta_emb_aver = delta_emb_all[:-1].mean(dim=0) # [2048]
+            delta_emb_tar = config['v'] * delta_emb_all[-1] # [2048]
+            
             oneactor_extra_config = {
                 'token_ids': token_id,
                 'delta_embs': delta_emb_tar,
@@ -103,47 +133,37 @@ def main():
                 'eta_2': config['eta_2'],
                 'delta_emb_aver': delta_emb_aver
             }
-
-            image = pipeline_inference(pipeline, config, oneactor_extra_config)
+            image = pipeline_inference(
+                pipeline,
+                config['prompt'] + " " + config['add_prompts'][img_num],
+                config['neg_prompt'][img_num],
+                config, oneactor_extra_config)
             image = image.images[0]
-            image.save(out_root+f'OneActor.jpg')
-    elif config['only_step'] == 'best':
-        projector_path = config['pretrain_root'] + f'/weight/best-learned-projector.pth'
-        delta_emb_all = projector_inference(projector_path, h_tar, h_base, config['device']).to(config['device'])
+            image.save(out_root+f'/OneActor_'+config['file_name'][img_num]+'_step_'+'best'+'.jpg')
+        else:
+            steps_list = config['only_step']
+            for staps in steps_list:   
+                with torch.no_grad():
+                    projector_path = config['pretrain_root'] + f'/weight/learned-projector-steps-{steps}.pth'
+                    delta_emb_all = projector_inference(projector_path, h_tar, h_base, config['device']).to(config['device'])
 
-        delta_emb_aver = delta_emb_all[:-1].mean(dim=0) # [2048]
-        delta_emb_tar = config['v'] * delta_emb_all[-1] # [2048]
-        
-        oneactor_extra_config = {
-            'token_ids': token_id,
-            'delta_embs': delta_emb_tar,
-            'delta_steps': select_list,
-            'eta_2': config['eta_2'],
-            'delta_emb_aver': delta_emb_aver
-        }
-        image = pipeline_inference(pipeline, config, oneactor_extra_config)
-        image = image.images[0]
-        image.save(out_root+f'/OneActor.jpg')
-    else:
-        steps = config['only_step']
-            
-        with torch.no_grad():
-            projector_path = config['pretrain_root'] + f'/weight/learned-projector-steps-{steps}.pth'
-            delta_emb_all = projector_inference(projector_path, h_tar, h_base, config['device']).to(config['device'])
+                delta_emb_aver = delta_emb_all[:-1].mean(dim=0) # [2048]
+                delta_emb_tar = config['v'] * delta_emb_all[-1] # [2048]
 
-        delta_emb_aver = delta_emb_all[:-1].mean(dim=0) # [2048]
-        delta_emb_tar = config['v'] * delta_emb_all[-1] # [2048]
-
-        oneactor_extra_config = {
-            'token_ids': token_id,
-            'delta_embs': delta_emb_tar,
-            'delta_steps': select_list,
-            'eta_2': config['eta_2'],
-            'delta_emb_aver': delta_emb_aver
-        }
-        image = pipeline_inference(pipeline, config, oneactor_extra_config)
-        image = image.images[0]
-        image.save(out_root+f'/OneActor.jpg')
+                oneactor_extra_config = {
+                    'token_ids': token_id,
+                    'delta_embs': delta_emb_tar,
+                    'delta_steps': select_list,
+                    'eta_2': config['eta_2'],
+                    'delta_emb_aver': delta_emb_aver
+                }
+                image = pipeline_inference(
+                    pipeline, 
+                    config['prompt'] + " " + config['add_prompts'][img_num],
+                    config['neg_prompt'][img_num],
+                    config, oneactor_extra_config)
+                image = image.images[0]
+                image.save(out_root+f'/OneActor_'+config['file_name'][img_num]+'_step_'+steps+'.jpg')
 
 if __name__ == '__main__':
     main()
