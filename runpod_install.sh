89cc36178aa1eece4fef29523afa9e9b126610c7
Katya Adishcheva <eadishcheva@gmail.com>
2025-03-27 22:18:03 +0100

    add runpod insteller

---
+++ runpod_install.sh
@@ -0,0 +1,12 @@
+python -m venv oa_venv
+. oa_venv/bin/activate
+pip install ipykernel
+python -m ipykernel install --user --name=oa_venv --display-name "Python (oa_venv)"
+
+mkdir /workspace/.ssh
+ssh-keygen -f /workspace/.ssh/id_rsa
+cp /workspace/.ssh/* /root/.ssh/
+chmod 400 /root/.ssh/id_rsa.pub
+chmod 400 /root/.ssh/id_rsa
+
+pip install -r requirements.txt
