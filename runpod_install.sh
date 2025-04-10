d4dfb10e3f059e7f89a686e08272fd37816eb798
EkaterinaAdishcheva <eadishcheva@gmail.com>
2025-04-10 22:17:23 +0200

    Update runpod_install.sh

--- runpod_install.sh
+++ runpod_install.sh
@@ -36,3 +36,6 @@ pip install torch
 pip install Pillow
 deactivate ds
 
+sam2
+https://github.com/facebookresearch/sam2/blob/main/notebooks/image_predictor_example.ipynb
+https://github.com/facebookresearch/sam2/blob/main/notebooks/automatic_mask_generator_example.ipynb
